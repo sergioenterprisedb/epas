47fd3b02d9441e32467d335710edf696394fc920
Sergio Romera <sergio.romera@enterprisedb.com>
2024-11-13 19:03:38 +0100

    Update

--- password_profile/create_users_table.sh
+++ password_profile/create_users_table.sh
@@ -7,11 +7,12 @@ DB_HOST="your_db_host"
 DB_PORT="your_db_port"
 
 $(psql -U $DB_USER -h $DB_HOST -d $DB_NAME -p $DB_PORT -t -c "
+DROP TABLE customer_users;
 CREATE TABLE customer_users (
   username TEXT,
   email TEXT
 );
-INSERT INTO customer_users VALUES 'USER1', 'user1@test.com';
-INSERT INTO customer_users VALUES 'USER2', 'user2@test.com';
-INSERT INTO customer_users VALUES 'USER_TEST', 'user3@test.com';
+INSERT INTO customer_users VALUES ('USER1', 'user1@test.com');
+INSERT INTO customer_users VALUES ('USER2', 'user2@test.com');
+INSERT INTO customer_users VALUES ('TEST_USER', 'user3@test.com');
 ")
--- password_profile/send_mail_expiration_date.sh
+++ password_profile/send_mail_expiration_date.sh
@@ -6,25 +6,30 @@ DB_NAME="your_db_name"
 DB_HOST="your_db_host"
 DB_PORT="your_db_port"
 
-# Query database for users with expiry_date in 10 days
+# Query database for users with expiry_date in less than 10 days
 EXPIRING_USERS=$(psql -U $DB_USER -h $DB_HOST -d $DB_NAME -p $DB_PORT -t -c "
-SELECT tu.name, tu.email
+SELECT u.username, u.email
 FROM customer_users u
-JOIN dba_users du ON u.name = du.username
-WHERE du.expiry_date = CURRENT_DATE + INTERVAL '10 days';
+JOIN dba_users du ON u.username = du.username
+WHERE du.expiry_date < CURRENT_DATE + INTERVAL '10 days';
 ")
 
 # Loop through the results and send emails
 IFS=$'\n'
 for line in $EXPIRING_USERS; do
     NAME=$(echo $line | awk '{print $1}')
-    EMAIL=$(echo $line | awk '{print $2}')
+    EMAIL=$(echo $line | awk '{print $3}')
 
     SUBJECT="Account Expiry Notification"
-    MESSAGE="Dear $NAME,\n\nYour account will expire in 10 days. Please take the necessary actions to renew it.\n\nBest Regards,\nAdmin Team"
+    MESSAGE="Dear $NAME, Your account will expire in 10 days. \
+Please take the necessary actions to renew it. \
+The command to change the password is: \
+\
+ALTER USER <username> PASSWORD '<new_password>' replace '<old_password>'; \
+Best Regards, Admin Team"
 
     #echo -e "$MESSAGE" | mail -s "$SUBJECT" "$EMAIL"
-    echo "$EMAIL"
-    echo "$MESSAGE"
-    echo "$SUBJECT"
+    echo "email: $EMAIL"
+    echo "Subject: $SUBJECT"
+    echo "Message: $MESSAGE"
 done
